444d0b599db5f8afabac367e5b43d0e1047e3d80
Nasir Billoo <nasir.billoo@gmail.com>
2016-05-22 07:42:56 -0400

    Getting ready for NPM

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var JiraAPI = require('./JiraAPI');
-var jira = new FreshDeskAPI("ThisFakeEmail@gmail.com", "MyPass1234", 'https://company.atlassian.net/');
+var jira = new JiraAPI("ThisFakeEmail@gmail.com", "MyPass1234", 'https://company.atlassian.net/');
 jira.printMsg();
